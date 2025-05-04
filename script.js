e9d51c7ab53fd7b80b4fa89cb357b7aa053ce8fb
fionaantonio022 <antonioprincessfiona2@gmail.com>
2025-05-05 00:01:37 +0800

    Updated Code

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-
-document.addEventListener("DOMContentLoaded", function() {
-    VANTA.GLOBE({
-        el: "#vanta-bg",
-        mouseControls: true,
-        touchControls: true,
-        gyroControls: false,
-        minHeight: 200.00,
-        minWidth: 200.00,
-        scale: 1.00,
-        scaleMobile: 1.00
-      })
-   
+document.addEventListener("DOMContentLoaded", function () {
+  VANTA.GLOBE({
+    el: "#vanta-bg",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.0,
+    minWidth: 200.0,
+    scale: 1.0,
+    scaleMobile: 1.0
+  });
 });
